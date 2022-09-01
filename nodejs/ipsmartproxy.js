d610eb87953f550a0a39c14301ba731b7523d992
Noah <45659684+NoahDrucker@users.noreply.github.com>
2022-09-01 12:18:42 +0300

    Add files via upload

---
+++ nodejs/ipsmartproxy.js
@@ -0,0 +1,6 @@
+const sdk = require('api')('@smartproxy/v1.0#25e7913l1ow524w');
+
+sdk.auth('SPusername', 'SPpassword');
+sdk.realTimeExample({target: 'universal', parse: false, url: 'https://ip.smartproxy.com/'})
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
