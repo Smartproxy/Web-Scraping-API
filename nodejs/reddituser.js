73bb671d4af54dd7c7801cd13b60b6b1a3fc4085
Noah <45659684+NoahDrucker@users.noreply.github.com>
2022-09-27 14:10:27 +0300

    Add files via upload

---
+++ nodejs/reddituser.js
@@ -0,0 +1,11 @@
+const sdk = require('api')('@smartproxy/v1.0#25e7913l1ow524w');
+
+sdk.auth('SPusername', 'SPpassword');
+sdk.realTimeExample({
+  target: 'universal',
+  parse: false,
+  url: 'https://www.reddit.com/user/thegoldinthemountain/',
+  headless: 'html'
+})
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
