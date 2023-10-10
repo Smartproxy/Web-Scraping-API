8c3ebefb53c9de74e36e7be1fd6b19b494c2451a
Noah <45659684+NoahDrucker@users.noreply.github.com>
2023-10-10 14:07:28 +0300

    Update facebookpost.php

--- php/facebookpost.php
+++ php/facebookpost.php
@@ -7,7 +7,7 @@ $response = $client->request('POST', 'https://scraper-api.smartproxy.com/v2/scra
   'body' => '{"target":"universal","parse":false,"headless":"html","url":"https://www.facebook.com/zuck/posts/pfbid0HeY54v4LMcv2EMxDz5RvnWaR6swsGFWikzUbrsEFtvxu9n4GCx7zA2YTM69XdiYnl"}',
   'headers' => [
     'Accept' => 'application/json',
-    'Authorization' => 'Basic U1B1c2VybmFtZTpTUHBhc3N3b3Jk',
+    'Authorization' => 'Basic AUTH',
     'Content-Type' => 'application/json',
   ],
 ]);
