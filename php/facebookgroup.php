7786977f6ef8dffbe98d10b5e6494fcd6380746b
Noah <45659684+NoahDrucker@users.noreply.github.com>
2023-10-10 14:07:06 +0300

    Update facebookgroup.php

--- php/facebookgroup.php
+++ php/facebookgroup.php
@@ -7,7 +7,7 @@ $response = $client->request('POST', 'https://scraper-api.smartproxy.com/v2/scra
   'body' => '{"target":"universal","parse":false,"url":"https://www.facebook.com/groups/1394454774138066"}',
   'headers' => [
     'Accept' => 'application/json',
-    'Authorization' => 'Basic U1B1c2VybmFtZTpTUHBhc3N3b3Jk',
+    'Authorization' => 'Basic AUTH',
     'Content-Type' => 'application/json',
   ],
 ]);
