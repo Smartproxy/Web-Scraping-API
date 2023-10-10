ffe267ffaf503625cf7cdbdd54accc7840343faf
Noah <45659684+NoahDrucker@users.noreply.github.com>
2023-10-10 14:03:53 +0300

    Update facebookpost.php

--- php/facebookpost.php
+++ php/facebookpost.php
@@ -3,7 +3,7 @@ require_once('vendor/autoload.php');
 
 $client = new \GuzzleHttp\Client();
 
-$response = $client->request('POST', 'https://scrape.smartproxy.com/v1/tasks', [
+$response = $client->request('POST', 'https://scraper-api.smartproxy.com/v2/scrape', [
   'body' => '{"target":"universal","parse":false,"headless":"html","url":"https://www.facebook.com/zuck/posts/pfbid0HeY54v4LMcv2EMxDz5RvnWaR6swsGFWikzUbrsEFtvxu9n4GCx7zA2YTM69XdiYnl"}',
   'headers' => [
     'Accept' => 'application/json',
